be054a5d2bb4e478b84a857ed2847e3354ca1f67
shghygh001 <shghyghkrimi2923@gmail.com>
2024-01-27 13:54:39 +0330

    Create

--- index.php
+++ index.php
@@ -3,8 +3,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Weblog</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <div class="alert alert-primary">
+        سلام
+    </div>
 </body>
+  <script src="js/bootstrap.min.js"></script> 
+  <script src="js/jquery-3.5.1.min.js"></script> 
 </html>
