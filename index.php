503c20a06fd97b63cc6c2c2ee660126c55ffe081
shghygh001 <shghyghkrimi2923@gmail.com>
2024-01-27 15:10:41 +0330

    add menu bar

--- index.php
+++ index.php
@@ -7,10 +7,39 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="container">
-    <div>
-        سلام
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light float-right">
+  <a class="navbar-brand" href="#">وبلاگ</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">خانه <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">پروفایل</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          مقالات
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">برنامه نویسی</a>
+          <a class="dropdown-item" href="#">طراحی وب</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">بازی سازی</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2 placeholder" type="search" placeholder="سرچ" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">جستجو</button>
+    </form>
+  </div>
+</nav>
 </body>
+<script src="js/jquery-3.5.1.min.js"></script> 
   <script src="js/bootstrap.min.js"></script> 
-  <script src="js/jquery-3.5.1.min.js"></script> 
 </html>
