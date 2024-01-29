2d814d29b697a375b16aef7f2c5c49f588e37e2b
shghygh001 <shghyghkrimi2923@gmail.com>
2024-01-29 16:04:57 +0330

    register form

---
+++ page/register.php
@@ -0,0 +1,77 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weblog</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+<body>
+  <div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">وبلاگ</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">خانه <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">پروفایل</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          مقالات
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">برنامه نویسی</a>
+          <a class="dropdown-item" href="#">طراحی وب</a>
+          <a class="dropdown-item" href="#">بازی سازی</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0 mr-auto">
+      <input class="form-control mr-sm-2 placeholder" type="search" placeholder="چی میخوای؟" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">جستجو</button>
+    </form>
+  </div>
+</nav>
+  </div><br><br><br><br><br><br><br><br>
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-4"> </div>
+        <div class="col-12 col-lg-4">
+            <form method="post" class="register-form">
+                <input type="text" placeholder="نام کاربری">
+                <input type="email" placeholder="ایمیل">
+                <input type="number" placeholder="سن">
+                <input type="password" placeholder="رمر عبور"><br>
+                <input type="submit" value="ثبت نام" class="btn btn-primary   register-submit">
+            </form>
+        </div>
+        <div class="col-lg-4"> </div>
+    </div>
+  </div>
+  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  <footer>
+  <div class="footer1">
+    <div class="container">
+      <div class="row d-none d-lg-flex">
+      <div class="col-12 col-lg-6 foot "><br><br><br><br><br>
+        <form>
+          <input type="text" class="input-group" placeholder="ایمیل">
+          <input type="submit" class="btn btn-success" value="عضویت">
+        </form>
+      </div>
+      </div>
+    </div>
+  </div>
+</footer>
+</body>
+<script src="../js/jquery-3.5.1.min.js"></script> 
+  <script src="../js/bootstrap.min.js"></script> 
+</html>
