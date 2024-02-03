6e031bb223a0c27a4b3c2ec087c15ec59d1db3ca
shghygh001 <shghyghkrimi2923@gmail.com>
2024-02-03 20:13:51 +0330

    add panel admin

---
+++ PHP/admin/admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>ADMIN</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+    <ul class="nav nav-pills nav-fill">
+  <li class="nav-item">
+    <a class="nav-link" href="page/menu.php">منو</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="page/blog.php">وبلاگ</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="page/comment.php">کامنت ها</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+    </div>
+</div>
+</body>
+<script src="../../js/jquery-3.5.1.min.js"></script>
+<script src="../../js/bootstrap.min.js"></script>
+</html>
