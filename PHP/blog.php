168f5da409db0137a89c4690be379655f12a8685
shghygh001 <shghyghkrimi2923@gmail.com>
2024-02-09 13:06:39 +0330

    add post

--- PHP/blog.php
+++ PHP/blog.php
@@ -1,7 +1,22 @@
 <?php
 include 'db.php';
 
-echo $date = jdate("Y/n/j");
+if(isset($_POST['Record'])){
+
+    $title=$_POST['title'];
+    $image=$_POST['image'];
+    $content=$_POST['content'];
+    $writer=$_POST['writer'];
+    $date = time();
+
+    $result=$conn->prepare("INSERT INTO blog (title,image,content,writer,date) VALUES(?,?,?,?,?)");
+    $result->bindValue(1,$title);
+    $result->bindValue(2,$image);
+    $result->bindValue(3,$content);
+    $result->bindValue(4,$writer);
+    $result->bindValue(5,$date);
+    $result->execute();
+}
 
 $result=$conn->prepare("SELECT * FROM writers");
 $result->execute();
