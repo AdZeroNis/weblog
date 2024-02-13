4a1e94e9ad6161a82c9ba5ebefac6e352c86d622
shghygh001 <shghyghkrimi2923@gmail.com>
2024-02-13 18:26:32 +0330

    Views page

--- PHP/single.php
+++ PHP/single.php
@@ -10,3 +10,15 @@ $blogs=$blogs->fetch(PDO::FETCH_ASSOC);
 $result=$conn->prepare("SELECT * FROM writers");
 $result->execute();
 $writers=$result->fetchAll(PDO::FETCH_ASSOC);
+ 
+$idblog= $blogs['id'];
+$views=$conn->prepare("INSERT INTO view (post) VALUES (?)");
+$views->bindValue(1,$idblog);
+$views->execute();
+
+
+$result=$conn->prepare("SELECT COUNT(id) FROM view WHERE post=?");
+$result->bindValue(1,$idblog);
+$result->execute();
+$numviews=$result->fetch(PDO::FETCH_ASSOC);
+foreach($numviews as $numview){}
